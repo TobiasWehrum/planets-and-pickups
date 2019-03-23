168daf187ff3ea193732341a6528309b831b59e8
Tobias Wehrum <Tobias.Wehrum@dragonlab.de>
2019-03-23 20:50:11 +0100

    Halfway laser implementation. Doesn't feel good, but committing (in deactivated state) for potential later experiments.

---
+++ Assets/Scripts/PlayerLaser.cs
@@ -0,0 +1,35 @@
+using UnityEngine;
+
+namespace MiniPlanetDefense
+{
+    public class PlayerLaser : MonoBehaviour
+    {
+        [SerializeField] LineRenderer targetingLineRender;
+        [SerializeField] LineRenderer laserLineRenderer;
+        [SerializeField] float distance = 100;
+
+        void Awake()
+        {
+            Debug.Log(laserLineRenderer.widthMultiplier);
+            laserLineRenderer.enabled = false;
+        }
+        
+        void Update()
+        {
+            var playerPosition = transform.position;
+            var mousePosition = Camera.main.ScreenToWorldPoint(Input.mousePosition);
+            mousePosition.z = 0;
+            
+            var targetDirection = (mousePosition - playerPosition).normalized;
+            if (targetDirection.sqrMagnitude == 0)
+            {
+                targetDirection.x = 1;
+            }
+
+            var targetPosition = playerPosition + targetDirection * distance;
+        
+            targetingLineRender.SetPosition(0, playerPosition);
+            targetingLineRender.SetPosition(1, targetPosition);
+        }
+    }
+}
