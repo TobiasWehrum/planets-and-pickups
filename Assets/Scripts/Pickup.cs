1e456c152bc2b5b8c76295bb8c685d7c9945983d
Tobias Wehrum <Tobias.Wehrum@dragonlab.de>
2019-03-24 21:45:28 +0100

    Despawn pickups when out of range

--- Assets/Scripts/Pickup.cs
+++ Assets/Scripts/Pickup.cs
@@ -1,9 +1,30 @@
 using UnityEngine;
+using Zenject;
 
 namespace MiniPlanetDefense
 {
     public class Pickup : MonoBehaviour
     {
+        [SerializeField] float extraDespawnDistance = 40f;
+
+        [Inject] Constants constants;
+        
+        float despawnDistanceSqr;
+
+        void Awake()
+        {
+            despawnDistanceSqr = Mathf.Pow(constants.playfieldRadius + extraDespawnDistance, 2);
+        }
+        
+        void Update()
+        {
+            var distanceFromCenterSqr = transform.position.sqrMagnitude;
+            if (distanceFromCenterSqr >= despawnDistanceSqr)
+            {
+                Destroy(gameObject);
+            }
+        }
+        
         public void Collect()
         {
             Destroy(gameObject);
